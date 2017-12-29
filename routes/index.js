4d3b5f8569451c5ed7676f45cc5fffbbfbaf2b8d
mpiskunov <matthewpiskunov@yahoo.com>
2017-12-28 19:01:18 -0500

    Changed layout

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET index page */
 router.get('/', function (req, res) {
-  res.render('partials/index', { title: 'index', message: 'Hello there! I am the index' });
+  res.render('partials/index', { title: 'Petals & Nettles', message: 'Hello there! I am the index' });
 });
 
 /* GET about page */
 router.get('/about', function (req, res) {
-  res.render('partials/about', { title: 'about', message: 'Hello there! I am the about' });
+  res.render('partials/about', { title: 'About Us', message: 'Hello there! I am the about' });
 });
 
 /* GET shop page */
